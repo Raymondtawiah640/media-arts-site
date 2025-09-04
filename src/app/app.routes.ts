ab206b1903dbf3a42908ae0e78550aba6b5dd37e
Raymondtawiah640 <enterprisekiln@gmail.com>
2025-09-03 16:22:41 -0900

    modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { Home} from './home/home';
 
 export const routes: Routes = [
   { path: '', component: Home },  // default route
-  { path: 'home', component: Home}, // optional, same as default
+  { path: 'home', component: Home}, 
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+// optional, same as default
   // Add other routes here if needed, e.g. about, services, contact
 ];
