9fcc35fe199d4fd6ef77495e13ecc35579c355ca
Raymondtawiah640 <enterprisekiln@gmail.com>
2025-09-23 08:59:39 -0900

    modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { MotionGraphics } from './categories/motion-graphics/motion-graphics';
 import { Printing } from './categories/printing/printing';
 import { TShirtPrinting } from './categories/t-shirt-printing/t-shirt-printing';
 import { VideoProductionAndPhotoEditing } from './categories/video-production-and-photo-editing/video-production-and-photo-editing';
-import { Streaming } from './categories/others/streaming/streaming';
 import { PictureFraming } from './categories/others/picture-framing/picture-framing';
 import { OnlineAdvertisement } from './categories/others/online-advertisement/online-advertisement';
 import { SoundEngineering } from './categories/others/sound-engineering/sound-engineering';
@@ -24,7 +23,6 @@ export const routes: Routes = [
   {path: 'Printing', component: Printing},
   {path: 'T-ShirtPrinting', component: TShirtPrinting},
   {path: 'VideoProductionAndPhotoEditing', component: VideoProductionAndPhotoEditing},
-  {path: 'Streaming', component: Streaming},
   {path: 'PictureFraming', component: PictureFraming},
   {path: 'OnlineAdvertisement', component: OnlineAdvertisement},
   {path: 'SoundEngineering', component: SoundEngineering},
